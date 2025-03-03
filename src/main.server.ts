9ec3f41b98737b198a68f9289bb8bac060ba27fd
Olga Koshkina <koshkina_spb@mail.ru>
2025-03-03 17:16:18 +0300

    bug fix

--- src/main.server.ts
+++
@@ -1,6 +0,0 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import {AppModule} from './app/app.module';
-
-const bootstrap = () => platformBrowserDynamic().bootstrapModule(AppModule);
-
-export default bootstrap;
